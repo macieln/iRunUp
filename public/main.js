082a080fd4401a7816ac7b3831b2588046abc116
Nahum Maciel <nahumamaciel@gmail.com>
2021-01-13 16:28:49 -0800

    Started section view styling & the view builder function.

---
+++ public/main.js
@@ -0,0 +1,15 @@
+var bodyEl = document.getElementsByTagName('body')[0];
+
+console.log(bodyEl);
+
+function viewBuilder(viewType, sectionTitle) {
+
+    bodyEl.innerHTML = "";
+
+    var viewEl = document.createElement('section');
+    viewEl.innerHTML = `<header id="${viewType}"><h3>${sectionTitle}</h3></header>`;
+
+    bodyEl.appendChild(viewEl);
+};
+
+viewBuilder("inputType", "PUMP INFORMATION");
